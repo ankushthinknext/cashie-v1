98207109010a628801f326e82fe32010d3e48899
accofankush <emailfankush@gmail.com>
2021-09-06 10:49:19 +0530

    Main component developement

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,30 +1,12 @@
 import React, { useEffect, useState } from "react";
-import axios from "../config/axiosConfig";
-import moment from "moment";
-import qs from "query-string";
+
 import { Grid, Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { Bar } from "react-chartjs-2";
-const data = {
-	labels: [
-		"Sunday",
-		"Monday",
-		"Tuesday",
-		"Wednesday",
-		"Thrusday",
-		"Friday",
-		"Saturday",
-	],
-	datasets: [
-		{
-			label: "# of Votes",
-			data: [120, 19, 3, 5, 2, 3],
-			fill: true,
-			backgroundColor: "rgb(255, 99, 132)",
-			borderColor: "rgba(255, 99, 132, 0.2)",
-		},
-	],
-};
+import axios from "../config/axiosConfig";
+import { Line, Bar } from "react-chartjs-2";
+import moment from "moment";
+
+import qs from "query-string";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -46,42 +28,66 @@ function Main() {
 	});
 
 	const [dashboardData, setDashboardData] = useState(null);
-
 	useEffect(() => {
-		getDashboardData();
-	}, []);
-
-	function getDashboardData() {
 		axios(`transaction/dashboard?${qs.stringify(query)}`).then((response) =>
 			setDashboardData(response.data.data),
 		);
-	}
+	}, []);
+	console.log(dashboardData);
+	const dataset = {
+		labels: [
+			"Sunday",
+			"Monday",
+			"Tuesday",
+			"Wenesday",
+			"Thruday",
+			"Friday",
+			"Saturday",
+		],
+		datasets: [
+			{
+				label: "# of Votes",
+				data: dashboardData && [
+					dashboardData.items[0] && dashboardData.items[0].grandtotal,
+					dashboardData.items[1] && dashboardData.items[1].grandtotal,
+					dashboardData.items[2] && dashboardData.items[2].grandtotal,
+					dashboardData.items[3] && dashboardData.items[3].grandtotal,
+					dashboardData.items[4] && dashboardData.items[4].grandtotal,
+					dashboardData.items[5] && dashboardData.items[5].grandtotal,
+					dashboardData.items[6] && dashboardData.items[6].grandtotal,
+				],
+				fill: true,
+				backgroundColor: "rgb(255, 99, 132)",
+				borderColor: "rgba(255, 99, 132, 0.2)",
+			},
+		],
+	};
+	console.log(dataset);
+
 	return (
 		<div>
-			<h3>Dashboard</h3>
 			<Grid container>
 				<Grid item xs={12} lg={4}>
 					<Paper className={classes.paper}>
-						{" "}
-						Transaction - {dashboardData && dashboardData.count}
+						Transaction- {dashboardData && dashboardData.count}
 					</Paper>
 				</Grid>
 				<Grid item xs={12} lg={4}>
 					<Paper className={classes.paper}>
-						Income - {dashboardData && dashboardData.total.toFixed(2)}
+						Income- {dashboardData && dashboardData.total}
 					</Paper>
 				</Grid>
 				<Grid item xs={12} lg={4}>
 					<Paper className={classes.paper}>
-						Quantity - {dashboardData && dashboardData.qty}
+						Transaction- {dashboardData && dashboardData.qty}
 					</Paper>
 				</Grid>
 			</Grid>
 
 			<Grid container>
-				<Grid item sm={12} lg={6}>
+				<Grid item xs={12} lg={6}>
 					<Paper>
-						<Bar data={data} />
+						<Line data={dataset} />
 					</Paper>
 				</Grid>
 			</Grid>
