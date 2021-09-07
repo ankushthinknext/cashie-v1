2f6e3253603147bc37e4423367a4052b77a84cc9
accofankush <emailfankush@gmail.com>
2021-09-07 09:35:32 +0530

    Main Component compleated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,26 +5,12 @@ import qs from "query-string";
 import { Grid, Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { Bar } from "react-chartjs-2";
-const data = {
-	labels: [
-		"Sunday",
-		"Monday",
-		"Tuesday",
-		"Wednesday",
-		"Thrusday",
-		"Friday",
-		"Saturday",
-	],
-	datasets: [
-		{
-			label: "# of Votes",
-			data: [120, 19, 3, 5, 2, 3],
-			fill: true,
-			backgroundColor: "rgb(255, 99, 132)",
-			borderColor: "rgba(255, 99, 132, 0.2)",
-		},
-	],
-};
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -34,7 +20,6 @@ const useStyles = makeStyles((theme) => ({
 		margin: "5px",
 		padding: theme.spacing(2),
 		textAlign: "center",
-		height: "100px",
 	},
 }));
 
@@ -43,26 +28,53 @@ function Main() {
 	const [query, setQuery] = useState({
 		start: moment().startOf("week").format("llll"),
 		end: moment().endOf("week").format("llll"),
+		limit: 7,
 	});
 
 	const [dashboardData, setDashboardData] = useState(null);
+	const [recentTransactions, setRecentTransactions] = useState(null);
 
 	useEffect(() => {
 		getDashboardData();
 	}, []);
+	const data = {
+		labels: [
+			"Sunday",
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thrusday",
+			"Friday",
+			"Saturday",
+		],
+		datasets: [
+			{
+				label: "# of Votes",
+				data: [],
+				fill: true,
+				backgroundColor: "rgb(255, 99, 132)",
+				borderColor: "rgba(255, 99, 132, 0.2)",
+			},
+		],
+	};
 
 	function getDashboardData() {
 		axios(`transaction/dashboard?${qs.stringify(query)}`).then((response) =>
 			setDashboardData(response.data.data),
 		);
+
+		axios(`transaction?${qs.stringify(query)}`).then((response) =>
+			setRecentTransactions(response.data.data.transactions),
+		);
 	}
+	console.log(dashboardData);
+	console.log(recentTransactions);
 	return (
 		<div>
 			<h3>Dashboard</h3>
 			<Grid container>
 				<Grid item xs={12} lg={4}>
 					<Paper className={classes.paper}>
-						{" "}
 						Transaction - {dashboardData && dashboardData.count}
 					</Paper>
 				</Grid>
@@ -80,8 +92,35 @@ function Main() {
 
 			<Grid container>
 				<Grid item sm={12} lg={6}>
-					<Paper>
+					<Paper className={classes.paper}>
 						<Bar data={data} />
+					</Paper>
+				</Grid>
+				<Grid item sm={12} lg={6}>
+					<Paper className={classes.paper}>
+						<Table>
+							<TableHead>
+								<TableRow>
+									<TableCell>Reciept No</TableCell>
+									<TableCell align="left">Date</TableCell>
+									<TableCell align="right">Qty</TableCell>
+									<TableCell align="right">Total</TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{recentTransactions &&
+									recentTransactions.map((transaction) => (
+										<TableRow key={transaction._id}>
+											<TableCell>{transaction._id}</TableCell>
+											<TableCell>
+												{moment(transaction.createdAt).format("llll")}
+											</TableCell>
+											<TableCell>{transaction.items.length}</TableCell>
+											<TableCell>{transaction.grandtotal}</TableCell>
+										</TableRow>
+									))}
+							</TableBody>
+						</Table>
 					</Paper>
 				</Grid>
 			</Grid>
